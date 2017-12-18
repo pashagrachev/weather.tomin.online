62dbfafa90e3432f6eff2f0aea462ba16e2108c6
Vladislav Tomin <vlad@tomin.pro>
2017-12-18 22:42:52 +0400

    Added webhooks protection

--- src/Controllers/DeploymentController.php
+++ src/Controllers/DeploymentController.php
@@ -14,33 +14,44 @@ class DeploymentController {
         $file_handler = new StreamHandler("../logs/deployment.log");
         $logger->pushHandler($file_handler);
 
-        if(!empty($request->getParsedBody()['payload'])) {
-            $logger->info(serialize($request->getParsedBody()['payload']));
+        $header = getallheaders();
+        $hmac = hash_hmac('sha1', file_get_contents('php://input'), getenv('PAYLOAD_SECRET'));
 
-            $update = false;
-            $payload = $request->getParsedBody()['payload'];
-            if(empty($payload['commits'])) {
-                $update = true;
-            } else {
-                $branch = array_pop(explode("/", $payload['ref']));
-                if($branch === 'master') {
+        if (isset($header['X-Hub-Signature']) && $header['X-Hub-Signature'] === 'sha1='.$hmac) {
+            $logger->info('Webhook ' . $header['X-Hub-Signature'] . 'verified');
+
+            if(!empty($request->getParsedBody()['payload'])) {
+                $logger->info(serialize($request->getParsedBody()['payload']));
+
+                $update = false;
+                $payload = $request->getParsedBody()['payload'];
+                if(empty($payload['commits'])) {
                     $update = true;
+                } else {
+                    $branch = array_pop(explode("/", $payload['ref']));
+                    if($branch === 'master') {
+                        $update = true;
+                    }
                 }
+
+                if($update) {
+                    exec('cd ' . getenv('REPO_DIR') . ' && ' . getenv('GIT_BIN_PATH')  . ' fetch');
+                    exec('cd ' . getenv('REPO_DIR') . ' && GIT_WORK_TREE=' . getenv('MASTER_DIR') . ' ' . getenv('GIT_BIN_PATH')  . ' checkout -f master');
+                    $commit_hash = shell_exec('cd ' . getenv('REPO_DIR') . ' && ' . getenv('GIT_BIN_PATH')  . ' rev-parse --short HEAD');
+
+                    $logger->info('Deployed branch: ' .  @$branch . ' Commit: ' . $commit_hash);
+                }
+
+                return $response->withStatus(200)->withJson('Deployment completed');
+            } else {
+                $logger->error('Payload not found');
+
+                return $response->withStatus(400)->withJson('Payload not found');
             }
-
-            if($update) {
-                exec('cd ' . getenv('REPO_DIR') . ' && ' . getenv('GIT_BIN_PATH')  . ' fetch');
-                exec('cd ' . getenv('REPO_DIR') . ' && GIT_WORK_TREE=' . getenv('MASTER_DIR') . ' ' . getenv('GIT_BIN_PATH')  . ' checkout -f master');
-                $commit_hash = shell_exec('cd ' . getenv('REPO_DIR') . ' && ' . getenv('GIT_BIN_PATH')  . ' rev-parse --short HEAD');
-
-                $logger->info('Deployed branch: ' .  @$branch . ' Commit: ' . $commit_hash);
-            }
-
-            return $response->withStatus(200)->withJson('OK');
         } else {
-            $logger->error('Payload not found');
+            $logger->error('Webhook not valid');
 
-            return $response->withStatus(400)->withJson('Payload not found');
+            return $response->withStatus(403)->withJson('Webhook not valid');
         }
     }
 }
