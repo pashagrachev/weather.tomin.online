ca9c70472e2c8540116108100be12cb5832c37f8
Vladislav Tomin <vlad@tomin.pro>
2017-12-22 13:34:57 +0400

    Update debug

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -12,7 +12,12 @@ class HomeController
     public function __invoke(Request $request, Response $response) {
         $event = json_decode(file_get_contents('php://input'), true);
 
-        file_put_contents('debug.log', $event);
+        $logger = new Logger('bot');
+        $logger->pushProcessor(new UidProcessor);
+        $file_handler = new StreamHandler("../logs/bot.log");
+        $logger->pushHandler($file_handler);
+
+        $logger->info('Event ' . print_r($event));
 
         switch ($event['type']) {
             case 'confirmation':
